75948ada7e808a41f6954be00e6974071ee3bf3a
Rob Ross <rross@usgs.gov>
2016-02-05 11:21:31 -0700

    fork edit

--- creat.sh
+++ creat.sh
@@ -8,3 +8,4 @@
 And Dan has an amazing sweater.
 It makes him appear a Shockers fan.
 Only if he knows how to separate the chaff
+Now I am making changes after forking. Its not possible to fork oneself.
